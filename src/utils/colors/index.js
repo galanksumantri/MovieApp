d189b6d2e91202bb4bfad804e9f73976a897c2c9
Galang Sumantri <galanksumantri@gmail.com>
2022-07-04 06:59:31 +0700

    add theme

---
+++ src/utils/colors/index.js
@@ -0,0 +1,25 @@
+const mainColors = {
+  purple: '#564AA5',
+  orange: '#FBC263',
+  green1: '#5CB85F',
+  green2: '#DEF1DF',
+  grey1: '#E9E9E9',
+  grey2: '#D3D9FD',
+};
+
+export const colors = {
+  primary: mainColors.purple,
+  secondary: mainColors.orange,
+  white: 'white',
+  background: mainColors.purple,
+  border: mainColors.grey1,
+  background2: mainColors.grey2,
+  text: {
+    primary: '#000000',
+    secondary: mainColors.green1,
+  },
+  button: {
+    primary: mainColors.green1,
+    secondary: mainColors.green2,
+  },
+};
---
+++ src/utils/fonts/index.js
@@ -0,0 +1,15 @@
+export const fonts = {
+  primary: {
+    300: 'Helvetica-Light',
+    700: 'Helvetica-B',
+    400: 'Helvetica',
+  },
+  secondary: {
+    100: 'Roboto-Thin',
+    300: 'Roboto-Light',
+    400: 'Roboto-Regular',
+    500: 'Roboto-Medium',
+    700: 'Roboto-Bold',
+    900: 'Roboto-Black',
+  },
+};
